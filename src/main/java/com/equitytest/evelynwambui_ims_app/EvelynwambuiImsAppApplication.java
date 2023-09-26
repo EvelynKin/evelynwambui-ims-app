ce895f9371af341a947a4b0db688935e296148ee
Evelyn Wambui <wambuievelyn50@gmail.com>
2023-09-26 10:58:39 +0300

    Initial commit

---
+++ src/main/java/com/equitytest/evelynwambui_ims_app/EvelynwambuiImsAppApplication.java
@@ -0,0 +1,13 @@
+package com.equitytest.evelynwambui_ims_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvelynwambuiImsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvelynwambuiImsAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/equitytest/evelynwambui_ims_app/EvelynwambuiImsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equitytest.evelynwambui_ims_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvelynwambuiImsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
