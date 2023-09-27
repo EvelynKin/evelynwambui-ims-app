245966f0de741984135f0c01d7ac9c8031b57845
eve <wambuievelyn50@gmail.com>
2023-09-27 12:43:32 +0300

    fix: Merge conflicts

--- src/main/java/com/equitytest/evelynwambui_ims_app/service_impl/UserServiceImpl.java
+++ src/main/java/com/equitytest/evelynwambui_ims_app/service_impl/UserServiceImpl.java
@@ -31,6 +31,7 @@ import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Optional;
 import java.util.UUID;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -43,126 +44,134 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
-  private final UserRepository userRepository;
-  private final SystemUserRepository systemUserRepository;
-  private final AdminUserRepository adminUserRepository;
-  private final RegularUserRepository regularUserRepository;
+    private final UserRepository userRepository;
+    private final SystemUserRepository systemUserRepository;
+    private final AdminUserRepository adminUserRepository;
+    private final RegularUserRepository regularUserRepository;
 
-  private final JwtServiceImpl jwtServiceImpl;
-  private final SharedUtilitiesService sharedUtilitiesService;
-  private final PasswordEncoder passwordEncoder;
-  private final ProducerServiceImpl producerService;
+    private final JwtServiceImpl jwtServiceImpl;
+    private final SharedUtilitiesService sharedUtilitiesService;
+    private final PasswordEncoder passwordEncoder;
+    private final ProducerServiceImpl producerService;
 
-  @Value("${spring.kafka.inventory.consumer.group-id}")
-  private String INVENTORY_CONSUMER_ID;
-
-
-  public RequestResponse registerUser(UserManagementRequest userManagementRequest) {
+    @Value("${spring.kafka.inventory.consumer.group-id}")
+    private String INVENTORY_CONSUMER_ID;
 
 
-    String userRole = userManagementRequest.getUserRole();
-    boolean userExists = false;
+    public RequestResponse registerUser(UserManagementRequest userManagementRequest) {
 
-    if (UserType.SYSTEM_USER.getUserRole().equals(userRole)) {
-      Optional<SystemUser> user = systemUserRepository.findByUsername(userManagementRequest.getUsername());
-      userExists = user.isPresent();
+
+        String userRole = userManagementRequest.getUserRole();
+        boolean userExists = false;
+
+        if (UserType.SYSTEM_USER.getUserRole().equals(userRole)) {
+            Optional<SystemUser> user = systemUserRepository.findByUsername(userManagementRequest.getUsername());
+            userExists = user.isPresent();
+        }
+        if (UserType.SYSTEM_USER.getUserRole().equals(userRole)) {
+            Optional<AdminUser> user = adminUserRepository.findByUsername(userManagementRequest.getUsername());
+            userExists = user.isPresent();
+        }
+        if (UserType.SYSTEM_USER.getUserRole().equals(userRole)) {
+            Optional<RegularUser> user = regularUserRepository.findByUsername(userManagementRequest.getUsername());
+            userExists = user.isPresent();
+        }
+
+        if (userExists) {
+
+            return ConcreteRequestResponse.builder()
+                    .error(true)
+                    .responseCode(1)
+                    .message("User already exists with username : " + userManagementRequest.getUsername())
+                    .build();
+        }
+
+        if (userRepository.findByUsername(userManagementRequest.getUsername()).isPresent()) {
+            producerService.sendMessage(INVENTORY_CONSUMER_ID, userManagementRequest.getUsername() + " registration");
+
+            return ConcreteRequestResponse.builder()
+                    .error(true)
+                    .responseCode(1)
+                    .message("User already exists with username : " + userManagementRequest.getUsername())
+                    .build();
+        }
+
+
+        User user;
+        if (UserType.SYSTEM_USER.getUserRole().equals(userRole)) {
+            SystemUser systemUser = new SystemUser();
+            systemUser.setUserDescription(userManagementRequest.getUserDescription());
+            systemUser.setTwoFactorEnabled(userManagementRequest.getTwoFactorEnabled());
+            systemUser.setTwoFactorSecret(userManagementRequest.getTwoFactorSecret());
+            user = systemUser;
+
+        } else if (UserType.ADMIN_USER.getUserRole().equalsIgnoreCase(userRole)) {
+            AdminUser adminUser = new AdminUser();
+            adminUser.setAdminCreatedBy(userManagementRequest.getAdminCreatedBy());
+            adminUser.setUserDescription(userManagementRequest.getUserDescription());
+            adminUser.setTwoFactorEnabled(userManagementRequest.getTwoFactorEnabled());
+            adminUser.setTwoFactorSecret(userManagementRequest.getTwoFactorSecret());
+            user = adminUser;
+
+        } else if (UserType.REGULAR_USER.getUserRole().equalsIgnoreCase(userRole)) {
+            RegularUser regularUser = new RegularUser();
+            regularUser.setPhoneNumber(userManagementRequest.getPhoneNumber());
+            regularUser.setDateOfBirth(sharedUtilitiesService.parseToLocalDate(userManagementRequest.getDateOfBirth()));
+            user = regularUser;
+
+        } else {
+            return ResponseEntity.badRequest()
+                    .body(
+                            ConcreteRequestResponse.builder()
+                                    .error(true)
+                                    .responseCode(1)
+                                    .message("Invalid user type : " + userRole)
+                                    .build())
+                    .getBody();
+        }
+
+        // Update the shared fields
+        LocalDateTime localDateTime = LocalDateTime.now();
+        user.setUserId(generateUserId());
+        user.setFullName(userManagementRequest.getFullName());
+        user.setUsername(userManagementRequest.getUsername());
+        user.setPassword(passwordEncoder.encode(userManagementRequest.getPassword()));
+        user.setEmailAddress(userManagementRequest.getEmailAddress());
+        user.setEmailAddressVerified(false);
+        user.setCreatedAtTimestamp(localDateTime);
+        user.setUpdatedAtTimestamp(localDateTime);
+
+        userRepository.save(user); // Save User
+
+        HashMap<String, Object> additionalProperties = new HashMap<>();
+        additionalProperties.put("jwtToken", jwtServiceImpl.generateToken(user));
+
+        return ConcreteRequestResponse.builder()
+                .error(false)
+                .responseCode(0)
+                .message("User registration successful!")
+                .additionalProperties(additionalProperties)
+                .build();
     }
-    if (UserType.SYSTEM_USER.getUserRole().equals(userRole)) {
-      Optional<AdminUser> user = adminUserRepository.findByUsername(userManagementRequest.getUsername());
-      userExists = user.isPresent();
-    } if (UserType.SYSTEM_USER.getUserRole().equals(userRole)) {
-      Optional<RegularUser> user = regularUserRepository.findByUsername(userManagementRequest.getUsername());
-      userExists = user.isPresent();
+
+    @Override
+    public RequestResponse loginUser(@Valid UserManagementRequest userManagementRequest) {
+
+        producerService.sendMessage(INVENTORY_CONSUMER_ID, userManagementRequest.getUsername() + "login ");
+        return null;
     }
 
-
-    if (userExists) {
-    if (userRepository.findByUsername(userManagementRequest.getUsername()).isPresent()) {
-      producerService.sendMessage(INVENTORY_CONSUMER_ID, userManagementRequest.getUsername() +" registration");
-
-      return ConcreteRequestResponse.builder()
-          .error(true)
-          .responseCode(1)
-          .message("User already exists with username : " + userManagementRequest.getUsername())
-          .build();
+    @Override
+    public RequestResponse logoutUser(@Valid UserManagementRequest userManagementRequest) {
+        return null;
     }
 
-
-    User user;
-    if (UserType.SYSTEM_USER.getUserRole().equals(userRole)) {
-      SystemUser systemUser = new SystemUser();
-      systemUser.setUserDescription(userManagementRequest.getUserDescription());
-      systemUser.setTwoFactorEnabled(userManagementRequest.getTwoFactorEnabled());
-      systemUser.setTwoFactorSecret(userManagementRequest.getTwoFactorSecret());
-      user = systemUser;
-
-    } else if (UserType.ADMIN_USER.getUserRole().equalsIgnoreCase(userRole)) {
-      AdminUser adminUser = new AdminUser();
-      adminUser.setAdminCreatedBy(userManagementRequest.getAdminCreatedBy());
-      adminUser.setUserDescription(userManagementRequest.getUserDescription());
-      adminUser.setTwoFactorEnabled(userManagementRequest.getTwoFactorEnabled());
-      adminUser.setTwoFactorSecret(userManagementRequest.getTwoFactorSecret());
-      user = adminUser;
-
-    } else if (UserType.REGULAR_USER.getUserRole().equalsIgnoreCase(userRole)) {
-      RegularUser regularUser = new RegularUser();
-      regularUser.setPhoneNumber(userManagementRequest.getPhoneNumber());
-      regularUser.setDateOfBirth(sharedUtilitiesService.parseToLocalDate(userManagementRequest.getDateOfBirth()));
-      user = regularUser;
-
-    } else {
-      return ResponseEntity.badRequest()
-          .body(
-              ConcreteRequestResponse.builder()
-                  .error(true)
-                  .responseCode(1)
-                  .message("Invalid user type : " + userRole)
-                  .build())
-          .getBody();
+    /**
+     * Method to generate userId
+     *
+     * @return String
+     */
+    private String generateUserId() {
+        return "user-" + UUID.randomUUID();
     }
-
-    // Update the shared fields
-    LocalDateTime localDateTime = LocalDateTime.now();
-    user.setUserId(generateUserId());
-    user.setFullName(userManagementRequest.getFullName());
-    user.setUsername(userManagementRequest.getUsername());
-    user.setPassword(passwordEncoder.encode(userManagementRequest.getPassword()));
-    user.setEmailAddress(userManagementRequest.getEmailAddress());
-    user.setEmailAddressVerified(false);
-    user.setCreatedAtTimestamp(localDateTime);
-    user.setUpdatedAtTimestamp(localDateTime);
-
-    userRepository.save(user); // Save User
-
-    HashMap<String, Object> additionalProperties = new HashMap<>();
-    additionalProperties.put("jwtToken", jwtServiceImpl.generateToken(user));
-
-    return ConcreteRequestResponse.builder()
-        .error(false)
-        .responseCode(0)
-        .message("User registration successful!")
-        .additionalProperties(additionalProperties)
-        .build();
-  }
-
-  @Override
-  public RequestResponse loginUser(@Valid UserManagementRequest userManagementRequest) {
-
-    producerService.sendMessage(INVENTORY_CONSUMER_ID, userManagementRequest.getUsername() + "login ");
-    return null;
-  }
-
-  @Override
-  public RequestResponse logoutUser(@Valid UserManagementRequest userManagementRequest) {
-    return null;
-  }
-
-  /**
-   * Method to generate userId
-   *
-   * @return String
-   */
-  private String generateUserId() {
-    return "user-" + UUID.randomUUID();
-  }
 }
